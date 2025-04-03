9a19d0672965186f67591c9587965a70eb095f84
adhham <adhham16@gmail.com>
2025-04-03 11:23:42 +0400

    second commit

--- next.config.mjs
+++ next.config.mjs
@@ -5,9 +5,22 @@ const withMDX = mdx({
   options: {},
 });
 
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+// };
+
+// export default withMDX(nextConfig);
+
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
   pageExtensions: ["ts", "tsx", "md", "mdx"],
+  assetPrefix: isProd ? '/your-repository-name/' : '',
+  basePath: isProd ? '/your-repository-name' : '',
+  output: 'export'
 };
 
 export default withMDX(nextConfig);
